fa3601410de3d4e0af0cf188bdc5cd119bd2c11a
lucashsu95 <lucashsu95@gmail.com>
2024-12-08 18:45:32 +0800

    Remove Day10

--- Day10/src/main/java/com/example/usertodoapi/infrastructure/repository/UserJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.usertodoapi.infrastructure.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.usertodoapi.domain.entity.User;
-
-public interface UserJpaRepository extends JpaRepository<User, Integer> { }
