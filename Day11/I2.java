d53548c5c1f4b576e730b60de67268e32589d47a
lucashsu95 <lucashsu95@gmail.com>
2025-06-19 14:15:02 +0800

    Update I2.java and Add java stream操作整理

--- Day11/I2.java
+++ Day11/I2.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class I2 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        boolean isPrime = true;
-        for (int i = 2; i < num; i++) {
-            if (num % i == 0){
-                isPrime = false;
-                break;
-            }
-        }
+        // boolean isPrime = true;
+        // for (int i = 2; i < num; i++) {
+        //     if (num % i == 0){
+        //         isPrime = false;
+        //         break;
+        //     }
+        // }
+        boolean isPrime = num > 1 && IntStream.range(2, num).noneMatch(i -> num % i == 0);
         System.out.println(isPrime ? "T" : "F");
         sc.close();
     }
